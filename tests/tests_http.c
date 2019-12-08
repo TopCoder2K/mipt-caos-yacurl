f42a45988f5ffd351ba5a7bd2fc4e3c10b5b577a
Dmitrii <demulkup@ya.ru>
2019-12-08 21:21:06 +0300

    [tests_http] for http_request

--- tests/tests_http.c
+++ tests/tests_http.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "http.h"
+#include "../src/http.h"
 
 const char test_case_1[] =
     "Host: mipt.ru\n"
@@ -9,7 +10,8 @@ const char test_case_1[] =
     "X-CSRF-Token:8000000000000009\n"
 ;
 
-int main() {
+void test_1() {
+    printf("Test 1\n");
     list_t *headers = NULL;
     int headers_count = http_parse_headers(
         test_case_1,
@@ -23,7 +25,16 @@ int main() {
         list_t *cur_header = headers->next;
         while (cur_header != NULL) {
             http_header_t *ps_header = cur_header->value;
-            printf("\"%s\": \"%s\"\n", ps_header->key, ps_header->value);
+            const char *key;
+            if (ps_header->key.k_code == HTTP_HDR_OTHER) {
+                printf("[UNK] ");
+                key = ps_header->key.k_str;
+            }
+            else {
+                printf("[%3d] ", ps_header->key.k_code);
+                key = http_header_str(ps_header->key.k_code);
+            }
+            printf(" \"%s\": \"%s\"\n", key, ps_header->value);
             cur_header = cur_header->next;
         }
         
@@ -32,6 +43,55 @@ int main() {
     else {
         printf("Error processing headers: %d\n", headers_count);
     }
+    printf("End test 1\n");
+}
+
+void test_2() {
+    printf("Test 2\n");
+    http_request_t *request = http_request_init();
+
+    http_header_t *hdr_host = malloc(sizeof(http_header_t));
+    hdr_host->key.k_code = HTTP_HDR_HOST;
+    hdr_host->value = strdup("ident.me");
+    http_request_sethdr(request, hdr_host);
+    hdr_host = NULL;
+
+    http_header_t *hdr_accept_encoding = malloc(sizeof(http_header_t));
+    hdr_accept_encoding->key.k_code = HTTP_HDR_OTHER;
+    hdr_accept_encoding->key.k_str = strdup("Accept-Encoding");
+    hdr_accept_encoding->value = strdup("gzip");
+    http_request_sethdr(request, hdr_accept_encoding);
+    hdr_accept_encoding = NULL;
+
+    http_header_t *hdr_user_agent = malloc(sizeof(http_header_t));
+    hdr_user_agent->key.k_code = HTTP_HDR_USER_AGENT;
+    hdr_user_agent->value = strdup("yacurl");
+    http_request_sethdr(request, hdr_user_agent);
+    hdr_user_agent = NULL;
+
+    list_t *cur_header = request->headers->next;
+    while (cur_header != NULL) {
+        http_header_t *ps_header = cur_header->value;
+        const char *key;
+        if (ps_header->key.k_code == HTTP_HDR_OTHER) {
+            printf("[UNK]");
+            key = ps_header->key.k_str;
+        }
+        else {
+            printf("[%3d]", ps_header->key.k_code);
+            key = http_header_str(ps_header->key.k_code);
+        }
+        printf(" \"%s\": \"%s\"\n", key, ps_header->value);
+        cur_header = cur_header->next;
+    }
+    
+    http_request_free(request);
+    printf("End test 2\n");
+}
+
+int main() {
+    test_1();
+    test_2();
     
     return 0;
 }
