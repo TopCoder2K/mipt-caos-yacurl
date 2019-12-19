7b72e287b4b856a903b0c171177545c9aa6c4920
TopCoder2K <pchelintsev.sv@phystech.edu>
2019-12-19 10:38:12 +0300

    Debugging sources were implemented.

---
+++ src/Debug/test_net.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "net.h"
+
+
+
+const unsigned int BUF_SIZE = 4096;
+
+    /*char *hostname;
+    int port;
+    char *send_buf;
+    size_t send_buf_size;
+    char *recv_buf;
+    size_t recv_buf_size;
+    void (*on_data)(const net_request_t *, size_t);*/
+
+int main(int argc, char **argv) {
+    char hostname[] = "localhost";
+    int port = 8000;
+    char *to_send = calloc(BUF_SIZE, sizeof(char));
+    size_t send_buf_size = BUF_SIZE;
+    char *to_recv = calloc(BUF_SIZE, sizeof(char));
+    size_t recv_buf_size = BUF_SIZE;
+
+    send_buf_size = read(STDIN_FILENO, to_send, BUF_SIZE);
+
+    net_request_t *net_req_ptr = net_request_init();
+    net_req_ptr->hostname = hostname;
+    net_req_ptr->port = port;
+    net_req_ptr->send_buf = to_send;
+    net_req_ptr->send_buf_size = send_buf_size;
+    net_req_ptr->recv_buf = to_recv;
+    net_req_ptr->recv_buf_size = recv_buf_size;
+    net_req_ptr->on_data = NULL;
+
+    int status = net_send_receive(net_req_ptr);
+    if (status == -1) {
+        return 1;
+    }
+    printf("GOT:\n%s\n", net_req_ptr->recv_buf);
+
+    net_request_free(net_req_ptr);
+    free(to_send);
+    free(to_recv);
+
+    return 0;
+}
---
+++ src/Debug/test_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from socket import socket, AF_INET, SOCK_STREAM, SOMAXCONN, SHUT_RDWR
+import sys
+
+def process_connection(conn):
+    buf_size = 4096 * 2
+
+    data = conn.recv(buf_size)
+    print("Got:\n{}\n".format(data))
+    conn.send(data)
+
+    conn.shutdown(SHUT_RDWR)
+    conn.close()
+
+
+if __name__ == "__main__":
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((ip, port))
+    sock.listen(SOMAXCONN)
+    while True:
+        conn, _ = sock.accept()
+        process_connection(conn)
